4c2757264d9b294bee7e7245a1be543cc25e91ea
Thomaz Miranda <43679181+tomytp@users.noreply.github.com>
2022-11-21 23:47:37 -0300

    BuscaPorNumero

--- test_ModuloPM.py
+++ test_ModuloPM.py
@@ -1,22 +1,41 @@
-from modulo_busca import busca 
+from modulo_busca import *
 
-def test_busca1():
-    assert busca('coca', 'supermercado') == (2, 'coca', 3, 42)
-def test_busca2():
-    assert busca('coca', '') == None
-def test_busca3():
-    assert busca('coca', 'vulc') == None
-def test_busca4():
-    assert busca('coca', None) == None
-def test_busca5():
-    assert busca('', 'supermercado') == None
-def test_busca6():
-    assert busca('', '') == None
-def test_busca7():
-    assert busca('Pneu', 'vulc') == None
-def test_busca8():
-    assert busca('Pneu', 'supermercado') == None
-def test_busca9():
-    assert busca(None, None) == None
-def test_busca10():
-    assert busca('Pneu', 'supermercado') == None
+def test_busca_nome1():
+    assert busca_nome('coca', 'supermercado') == (2, 'coca', 3, 42)
+def test_busca_nome2():
+    assert busca_nome('coca', '') == None
+def test_busca_nome3():
+    assert busca_nome('coca', 'vulc') == None
+def test_busca_nome4():
+    assert busca_nome('coca', None) == None
+def test_busca_nome5():
+    assert busca_nome('', 'supermercado') == None
+def test_busca_nome6():
+    assert busca_nome('', '') == None
+def test_busca_nome7():
+    assert busca_nome('Pneu', 'vulc') == None
+def test_busca_nome8():
+    assert busca_nome('Pneu', 'supermercado') == None
+def test_busca_nome9():
+    assert busca_nome(None, None) == None
+
+
+
+def test_busca_numero1():
+    assert busca_numero('2', 'supermercado') == (2, 'coca', 3, 42)
+def test_busca_numero2():
+    assert busca_numero('2', '') == None
+def test_busca_numero3():
+    assert busca_numero('2', None) == None
+def test_busca_numero4():
+    assert busca_numero('2', 'vulc') == None
+def test_busca_numero5():
+    assert busca_numero('', 'supermercado') == None
+def test_busca_numero6():
+    assert busca_numero('', '') == None
+def test_busca_numero7():
+    assert busca_numero('850', 'vulc') == None
+def test_busca_numero8():
+    assert busca_numero('850', 'supermercado') == None
+def test_busca_numero9():
+    assert busca_numero(None, None) == None
