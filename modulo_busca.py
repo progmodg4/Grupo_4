7978a2e3ec56e82e8456d4226a4d5ac8f4330506
Thomaz Miranda <43679181+tomytp@users.noreply.github.com>
2022-11-21 23:36:38 -0300

    Added test support

---
+++ modulo_busca.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+def busca(nome, BancoDados):
+
+    con = sqlite3.connect("supermarket.db")
+    cursor = con.cursor()
+
+    try:
+        cursor.execute(f'''SELECT * FROM {BancoDados} WHERE Nome_do_produto  = {f"'{nome}'"}''')
+        #faz a busca em sql
+    except:
+        return None
+
+    rows = cursor.fetchall()
+    #pega o resultado do select
+    for row in rows:
+        return row
